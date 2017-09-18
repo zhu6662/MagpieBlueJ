52dd27c8870ce89acacc8fc0f7aa44da2ec3e490
Michael Zhu <Zhu56662>
2017-09-18 10:27:43 -0700

    Meg3

--- 1.1.6MagpieActivity2_StarterCode_BlueJ/1.1.6MagpieActivity2_Starter/Magpie2.java
+++ 1.1.6MagpieActivity2_StarterCode_BlueJ/1.1.6MagpieActivity2_Starter/Magpie2.java
@@ -21,6 +21,79 @@ public class Magpie2
         return "Hello, let's talk.";
     }
     
+    
+    /**
+     * Search for one word in phrase. The search is not case
+     * sensitive. This method will check that the given goal
+     * is not a substring of a longer string (so, for
+     * example, "I know" does not contain "no").
+     * 
+     * @param statement
+     *            the string to search
+     * @param goal
+     *            the string to search for
+     * @param startPos
+     *            the character of the string to begin the
+     *            search at
+     * @return the index of the first occurrence of goal in
+     *         statement or -1 if it's not found
+     */
+    private int findKeyword(String statement, String goal,
+    int startPos)
+    {
+        String phrase = statement.trim();
+        // The only change to incorporate the startPos is in
+        // the line below
+        int psn = phrase.toLowerCase().indexOf(
+                goal.toLowerCase(), startPos);
+        int iteration = 1;
+        // Refinement--make sure the goal isn't part of a
+        // word
+        while (psn >= 0)
+        {
+            // Find the string of length 1 before and after
+            // the word
+            String before = " ", after = " ";
+            if (psn > 0)
+            {
+                before = phrase.substring(psn - 1, psn)
+                .toLowerCase();
+            }
+            if (psn + goal.length() < phrase.length())
+            {
+                after = phrase.substring(
+                    psn + goal.length(),
+                    psn + goal.length() + 1)
+                .toLowerCase();
+            }
+            System.out.println("iteration: " + iteration);
+            System.out.println("psn: " + psn);
+            System.out.println("before: " + before);
+            System.out.println("after: " + after);
+            // If before and after aren't letters, we've
+            // found the word
+            if (((before.compareTo("a") < 0) || (before
+                    .compareTo("z") > 0)) // before is not a
+                // letter
+            && ((after.compareTo("a") < 0) || (after
+                    .compareTo("z") > 0)))
+            {
+                return psn;
+            }
+            iteration++;
+
+            // The last position didn't work, so let's find
+            // the next, if there is one.
+            psn = phrase.indexOf(goal.toLowerCase(),
+                psn + 1);
+
+        }
+
+        return -1;
+    }
+
+    
+    
     /**
      * Gives a response to a user statement
      * 
@@ -39,40 +112,40 @@ public class Magpie2
         }
         if(empty)
             return "Say something, please.";
-        if (statement.indexOf("no") >= 0)
+        if (findKeyword(statement,"no",0) >= 0)
         {
             response = "Why so negative?";
         }
-        else if (statement.indexOf("mother") >= 0
-                || statement.indexOf("father") >= 0
-                || statement.indexOf("sister") >= 0
-                || statement.indexOf("brother") >= 0)
+        else if (findKeyword(statement,"mother",0) >= 0
+                || findKeyword(statement,"father",0) >= 0
+                || findKeyword(statement,"sister",0) >= 0
+                || findKeyword(statement,"brother",0) >= 0)
         {
             response = "Tell me more about your family.";
         }
-        else if (statement.indexOf("dog") >=0 
-                    || statement.indexOf("cat") >=0 )
+        else if (findKeyword(statement,"dog",0) >=0 
+                    || findKeyword(statement,"cat",0) >=0 )
         {
             response = "Tell me more about you pets.";
         }
-        else if (statement.indexOf("Mr.") >= 0 ) 
+        else if (findKeyword(statement,"Mr.",0) >= 0 ) 
         {
             response = "He sounds like a good teacher";
         }
-        else if (statement.indexOf("Mrs.") >=0
-                || statement.indexOf("Ms.") >=0)
+        else if (findKeyword(statement,"Mrs.",0) >=0
+                || findKeyword(statement,"Ms.",0) >=0)
         {
             response = "She sounds like a good teacher";
         }
-        else if (statement.indexOf("You") >= 0 ) 
+        else if (findKeyword(statement,"You",0) >= 0 ) 
         {
             response = "I am Magpie.";
         }
-        else if (statement.indexOf("Why") >= 0 ) 
+        else if (findKeyword(statement,"Why",0) >= 0 ) 
         {
             response = "I don’t know. Do you know?";
         }
-        else if (statement.indexOf("Where") >= 0 ) 
+        else if (findKeyword(statement,"Where",0) >= 0 ) 
         {
             response = "It is probably somewhere in Canada";
         }
@@ -118,6 +191,6 @@ public class Magpie2
         {
             response = "Amazing. How was your day today?";
         }
-	return response;
+    return response;
     }
 }
